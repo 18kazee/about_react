cda413b8b5760a82ffb9766d079c8fdf816eeec7
18kazee <18kazee2415@gmail.com>
2023-09-28 01:38:53 +0900

    jsx記法はjsの中にhtmlを書いていく記法のこと

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return null;
+  return <h1>こんにちは!</h1>
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
